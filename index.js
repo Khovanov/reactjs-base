0a9a1da410da146bafeccc48387933dc3ff11cf6
Stanislav Khovanov <stanislav.khovanov@gmail.com>
2018-07-08 17:34:27 +0300

    create Calculator class

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './src/Hello';
+import Calculator from './src/Calculator';
+
+let calc = new Calculator();
+console.log('RESULT 1+2 =', calc.sum(1, 2));
+console.log('RESULT 2 pow 3 =', calc.pow(2, 3));
 
 ReactDOM.render(
   <Hello />,
   document.getElementById('app')
-)
+);
---
+++ src/Calculator.js
@@ -0,0 +1,6 @@
+class Calculator {
+  sum(a, b) { return a + b; }
+  pow(base, exponent) { return Math.pow(base, exponent); }
+}
+
+export default Calculator;
