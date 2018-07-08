ac1d723157781b088899f5be36fa5228d0195cb2
Stanislav Khovanov <stanislav.khovanov@gmail.com>
2018-07-08 17:45:23 +0300

    display Calculator result from Component

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Label from './Label';
+import Calculator from './Calculator';
 
 class Hello extends Component {
   render() {
+    let calc = new Calculator();
     return (
-      <div>Hi <Label>React</Label></div>
+      <div>Sum 1 and 2 is <Label>{calc.sum(1, 2)}</Label></div>
     )
   }
 }
