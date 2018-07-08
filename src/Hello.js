ce5b3a3a1c7c179558f5c53aa8a013c9ce45b9cd
Stanislav Khovanov <stanislav.khovanov@gmail.com>
2018-07-08 13:34:44 +0300

    create simple React application with Hello and Label components

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Label from './Label';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>Hi <Label>React</Label></div>
+    )
+  }
+}
+
+export default Hello;
---
+++ src/Label.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Label extends Component {
+  render() {
+    const children = this.props.children;
+    return (
+      <span style={{ color: 'red'}}>{children}</span>
+    )
+  }
+}
+
+export default Label;
